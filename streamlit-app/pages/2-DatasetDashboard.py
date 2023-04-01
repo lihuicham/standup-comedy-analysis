c2971cd21ffbe93576e1eb2fe77dfc0b71ad369f
Li Hui Cham <lihuicham@gmail.com>
2023-04-01 16:00:55 +0800

    uncomment wordcloud

--- streamlit-app/pages/2-DatasetDashboard.py
+++ streamlit-app/pages/2-DatasetDashboard.py
@@ -80,20 +80,20 @@ st.plotly_chart(fig_most_common)
 
 # EDA 2
 # comment out first 
-# st.markdown('#### Word Cloud')
-# st.markdown('Transcripts are cleaned, no stopwords, no most common words and etc. Word cloud should be meaningful')
+st.markdown('#### Word Cloud')
+st.markdown('Transcripts are cleaned, no stopwords, no most common words and etc. Word cloud should be meaningful')
 
-# tf_matrix = pd.read_pickle('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/pickle/tfidf_stop.pkl')
-# tf_matrix = tf_matrix.transpose()
+tf_matrix = pd.read_pickle('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/pickle/tfidf_stop.pkl')
+tf_matrix = tf_matrix.transpose()
 
-# transcript_num = st.slider('Select transcript to view word cloud : ', 0, 414, 60)
+transcript_num = st.slider('Select transcript to view word cloud : ', 0, 414, 60)
 
-# wc = WordCloud().generate_from_frequencies(tf_matrix[transcript_num])
+wc = WordCloud().generate_from_frequencies(tf_matrix[transcript_num])
 
-# fig_wc1, ax = plt.subplots(figsize = (12, 8))
-# ax.imshow(wc)
-# plt.axis("off")
-# st.pyplot(fig_wc1)
+fig_wc1, ax = plt.subplots(figsize = (12, 8))
+ax.imshow(wc)
+plt.axis("off")
+st.pyplot(fig_wc1)
 
 
 
