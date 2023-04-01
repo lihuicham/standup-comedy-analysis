0d96a3f9e79b2ec20a5e0635c434952e4f00f313
Li Hui Cham <lihuicham@gmail.com>
2023-04-01 14:52:39 +0800

    basic BackgroundDashboard completed

--- streamlit-app/HomeDashboard.py
+++ streamlit-app/HomeDashboard.py
@@ -1,8 +1,19 @@
 import streamlit as st
-import pandas as pd
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+'''
+# BT4222 Final Project 
+## Stand-up Comedy Analysis 
+
+### Links : 
+[GitHub Repo & Code](https://github.com/lihuicham/standup-comedy-analysis)  
+[Streamlit Dashboard App]()  
+[Final Project Report]()  
+[Presentation Slides]()  
+
+### Contributors 
+[Cham Li Hui (@lihuicham)](https://github.com/lihuicham)  
+[Chloe Ong Xi En (@chloeoxe)](https://github.com/chloeoxe)  
+[Clifford Tan Zhanhui (@cliffordtzh)](https://github.com/cliffordtzh)  
+[Regina Ho Xue Fang (@reginahoxf)](https://github.com/reginahoxf)  
+[Tan Li Hui, Faith (@faithtanlh)](https://github.com/faithtanlh)  
+'''
--- streamlit-app/pages/1-BackgroundDashboard.py
+++ streamlit-app/pages/1-BackgroundDashboard.py
@@ -1,3 +1,38 @@
+import streamlit as st
+import pandas as pd 
+import plotly.express as px
+
+df = pd.read_csv('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/transcripts.csv')
+df = df[df.columns[1:]]
+
+# create a word count column
+df['Comedian'] = df['Comedian'].astype('str')
+df['Comedian'] = df['Comedian'].apply(lambda x: x.lower())
+df['Word Count'] = df['Transcript'].str.split().str.len()
 
 
 
+comedian_count_df = df['Comedian'].value_counts().rename_axis('Comedian').reset_index(name='counts')
+comedian_count_df = comedian_count_df.iloc[1: , :]  # drop the nan row
+fig_comedian = px.bar(comedian_count_df, x="Comedian", y="counts", color="counts", title="Most Common Comedians in Website")
+st.plotly_chart(fig_comedian)
+
+top_10_list = list(comedian_count_df.head(10)['Comedian'])
+
+top_10_comedians = ", ".join(top_10_list)
+st.write('The 10 most common comedians on the website are : ', top_10_comedians)
+
+st.write('Let\'s look at their word count per standup-comedy show !')
+
+selected_comedian = st.selectbox(
+    'Comedians',
+    top_10_list)
+
+selected_df = df[(df.Comedian == selected_comedian)]
+
+st.write(selected_comedian, 'has ', len(selected_df.index), ' shows.')
+
+
+selected_df = selected_df.drop(['Date', 'Subtitle', 'Transcript'], axis=1)
+
+st.dataframe(selected_df)
