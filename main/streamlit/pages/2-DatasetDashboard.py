6790e480d370272c1a846b631d6a8ded97953d68
Li Hui Cham <lihuicham@gmail.com>
2023-04-01 11:31:47 +0800

    basic dataset and preprocessing visualisation done

--- main/streamlit/pages/2-DatasetDashboard.py
+++ main/streamlit/pages/2-DatasetDashboard.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+from wordcloud import WordCloud
 
 '''
 # Dataset Dashboard 
@@ -45,13 +49,51 @@ Reusing the simple sentence from Extra 1.
 
 '''
 ## Data Organization 
-After thorough data cleaning, our dats is organized in two ways : 
+After thorough data cleaning, our data is organized in two ways : 
 - Corpus : the "cleaned" `Transcript` column. 
 - Document-Term Matrix : **Count Vectorizer** and **TF-IDF Vectorizer**
 
 With more accurate predictions and more meaningful words, we chose the document-term matrix created with TF-IDF Vectorizer. 
 
-## Exploratory Data Analysis (EDA) 
-
+'''
 
 '''
+## Exploratory Data Analysis (EDA) 
+
+1. Most Common Words in all transcripts. (frequency >= 150)
+2. Word Cloud for 6 random transcripts (cleaned data)
+
+'''
+# EDA 1
+st.markdown('#### Most Common Words')
+
+most_common_words_complete = pd.read_pickle('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/pickle/mostcommonwords-st.pkl')
+most_common_words = [(word, freq) for word, freq in most_common_words_complete if freq >= 150]
+most_common_words_df = pd.DataFrame(most_common_words, columns = ['word' , 'count'])
+
+fig_most_common = px.bar(most_common_words_df, x='word', y='count')
+fig_most_common.update_layout(
+    title="Most Common Words in All Transcripts",
+    xaxis_title="Word",
+    yaxis_title="Count",
+)
+st.plotly_chart(fig_most_common)
+
+# EDA 2
+st.markdown('#### Word Cloud')
+st.markdown('Transcripts are cleaned, no stopwords, no most common words and etc. Word cloud should be meaningful')
+
+tf_matrix = pd.read_pickle('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/pickle/tfidf_stop.pkl')
+tf_matrix = tf_matrix.transpose()
+
+transcript_num = st.slider('Select transcript to view word cloud : ', 0, 414, 60)
+
+wc = WordCloud().generate_from_frequencies(tf_matrix[transcript_num])
+
+fig_wc1, ax = plt.subplots(figsize = (12, 8))
+ax.imshow(wc)
+plt.axis("off")
+st.pyplot(fig_wc1)
+
+
+
