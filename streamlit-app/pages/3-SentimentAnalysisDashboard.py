1e2282c60ac2806bbf1d1874d8ef4e939513c468
Li Hui Cham <lihuicham@gmail.com>
2023-04-03 02:34:55 +0800

    add transcript details for wordcloud

--- streamlit-app/pages/3-SentimentAnalysisDashboard.py
+++ streamlit-app/pages/3-SentimentAnalysisDashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px
 from textblob import TextBlob
-
+from empath import Empath
 
 '''
 # Sentiment Analysis Dashboard
@@ -58,4 +58,23 @@ fig_scatter.update_layout(
     yaxis_title="<-- Facts -------- Opinions -->",
 )
 st.plotly_chart(fig_scatter)
+
+'''
+## Topic Modelling
+We are using the [**Empath**](https://github.com/Ejhfast/empath-client) library for topic modelling. 
+
+Let's try this out ! 
+'''
+
+sent = st.text_input('Here\'s a sample sentence.', 'He hit the other person.')
+
+lexicon = Empath()
+
+def demo(sent) : 
+    lex_dict_test = lexicon.analyze(sent, normalize=True)
+    topics = sorted(lex_dict_test, key=lex_dict_test.get, reverse=True)[:5]
+    return ', '.join(topics)
+
+st.markdown('**Top 5 Topics :**')
+st.write(demo(sent))
 
