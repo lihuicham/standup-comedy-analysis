10e2a39eaabe00d697c3546d55dc13c3b979454e
Li Hui Cham <lihuicham@gmail.com>
2023-04-03 02:40:09 +0800

    added topic modeling to demodashboard

--- streamlit-app/pages/3-SentimentAnalysisDashboard.py
+++ streamlit-app/pages/3-SentimentAnalysisDashboard.py
@@ -59,22 +59,4 @@ fig_scatter.update_layout(
 )
 st.plotly_chart(fig_scatter)
 
-'''
-## Topic Modelling
-We are using the [**Empath**](https://github.com/Ejhfast/empath-client) library for topic modelling. 
-
-Let's try this out ! 
-'''
-
-sent = st.text_input('Here\'s a sample sentence.', 'He hit the other person.')
-
-lexicon = Empath()
-
-def demo(sent) : 
-    lex_dict_test = lexicon.analyze(sent, normalize=True)
-    topics = sorted(lex_dict_test, key=lex_dict_test.get, reverse=True)[:5]
-    return ', '.join(topics)
-
-st.markdown('**Top 5 Topics :**')
-st.write(demo(sent))
 
