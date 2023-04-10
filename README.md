bf76712d8c50e5213afa09c18672b3dfc774b2d5
Li Hui Cham <lihuicham@gmail.com>
2023-04-10 23:43:11 +0800

    update link

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Stand-Up Comedy Analysis
 
-[Streamlit Dashboard App](https://lihuicham-standup-come-streamlit-app-deployhomedashboard-16jw8i.streamlit.app/DemoDashboard)
+[Streamlit Dashboard App](https://lihuicham-standup-come-streamlit-app-deployhomedashboard-16jw8i.streamlit.app/)
--- streamlit-app/HomeDashboard.py
+++ streamlit-app/HomeDashboard.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 ### Links : 
 [GitHub Repo & Code](https://github.com/lihuicham/standup-comedy-analysis)  
-[Streamlit Dashboard App](https://lihuicham-standup-come-streamlit-app-deployhomedashboard-16jw8i.streamlit.app/DemoDashboard)  
+[Streamlit Dashboard App](https://lihuicham-standup-come-streamlit-app-deployhomedashboard-16jw8i.streamlit.app/)  
 
 ### Contributors 
 [Cham Li Hui (@lihuicham)](https://github.com/lihuicham)  
