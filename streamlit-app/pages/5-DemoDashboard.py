e462ab27738a65bd152c02c609b25729cf293756
Li Hui Cham <lihuicham@gmail.com>
2023-04-10 22:38:28 +0800

    basic demo dashboard done

--- streamlit-app/pages/5-DemoDashboard.py
+++ streamlit-app/pages/5-DemoDashboard.py
@@ -1,19 +1,18 @@
 import streamlit as st 
 import pandas as pd
 from empath import Empath
-from imblearn.over_sampling import SMOTE
-import pickle
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
+from nltk import word_tokenize, pos_tag
 from textblob import TextBlob
 import string
 import re
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import GradientBoostingClassifier
+from empath import Empath
+import numpy as np
+from imblearn.over_sampling import SMOTE
 
 ## helper functions from pipeline_functions.py
-
 def clean_text(text) :
     """Make text lowercase, remove text in square brackets, remove punctuations, 
     remove quotation marks, remove words containing numbers, remove \n"""
@@ -64,84 +63,144 @@ def filter_by_sentiment(data, sample, n = 10):
 
     return data.loc[n_closest, :].reset_index(drop = True)
 
+#function from utils
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        # As default pos in lemmatization is Noun
+        return wordnet.NOUN
 
-#function to preprocess transcript parts
-def preprocess(text):
-    stopwords_lst = stopwords.words('english')
+#function from utils
+def pos_then_lemmatize(pos_tagged_words):
     lemmatizer = WordNetLemmatizer()
-    add_punctuation = '“”’'
+
+    res = []
+    for pos in pos_tagged_words : 
+        word = pos[0]
+        pos_tag = pos[1]
+
+        lem = lemmatizer.lemmatize(word, get_wordnet_pos(pos_tag))
+        res.append(lem)
+    return res
+
+#function to preprocess transcript parts [heavily adapted from custom_tokenizer_stop(doc) in utils, and added digit removal]
+def preprocess(doc):
+    #remove all action text, meaning bracketed words
+    doc = re.sub('\[[a-zA-Z\s\-]+\]|\([a-zA-Z\s\-]+\)', '', doc)
 
     #remove punctuation
-    punctuation_free = "".join([i for i in text if i not in string.punctuation])
-    
+    punct = string.punctuation + "’“”–"
+    doc = doc.translate(str.maketrans(punct, " " * len(punct)))
+
     #remove digits
-    digits_removed = ''.join([i for i in punctuation_free if not i.isdigit()])
+    doc = ''.join([i for i in doc if not i.isdigit()])
+
+    #make doc lowercase and tokenize to words
+    words = word_tokenize(doc.lower())
     
-    #make words to lowercase
-    text_lower = digits_removed.lower()
+    #add our own stop word list to the existing English stop words 
+    stop_words = stopwords.words("english") + [
+        "get", "go", "know", "dont", "im", "like", "say", "thats", "one", "come", "right", "think", "youre", 
+        "people", "see", "look", "want", "time", "make", "na", "gon", "thing", "oh", "take", "good", "guy", 
+        "fuck", "would", "yeah", "tell", "well", "he", "shit", "cause", "back", "theyre", "man", "really", "cant", "little",
+        "let", "just", "okay", "ive", "♪", "–", "ta", "uh", "wan", "g", "e", "ah", "r", "mi", "le"
+    ]
     
-    #tokenize to words
-    tokenized = word_tokenize(text_lower)
-    
-    #remove stopwords
-    stopwords_removed = [i for i in tokenized if i not in stopwords_lst]
-    
-    #lemmatize
-    lemm_text = [lemmatizer.lemmatize(word) for word in stopwords_removed]
-    
+    #remove stopwords before lemmatization
+    filtered_words = [w.strip() for w in words if not w in stop_words] 
+
+    #pos tag words correctly and lemmatize them according to their corrected pos tag
+    pos_tagged_words = pos_tag(filtered_words)
+    pos_lemmatized_words = pos_then_lemmatize(pos_tagged_words)
+
+    #remove stopwords after lemmatization
+    filtered_words_2 = [w for w in pos_lemmatized_words if not w in stop_words] 
+
     #join list of words back into string
-    processed_text = ' '.join(lemm_text)
-    
-    #remove punctuations not found in string.punctuation
-    final_text = "".join([i for i in processed_text if i not in add_punctuation])
-    
+    final_text = ' '.join(filtered_words_2)
+
     return final_text
     
 '''
 # Demo 
 '''
 st.markdown('## Step 1 : User Input Transcript')
-st.markdown('Copy sample transcript [here](https://github.com/lihuicham/standup-comedy-analysis/blob/main/main/test_pipeline_transcript.txt).')
+st.markdown('Copy sample transcript [here](https://github.com/lihuicham/standup-comedy-analysis/blob/main/main/sample.txt). You can click the copy icon to copy the text !')
 user_input = st.text_area('Input the comedy transcript you would like to predict :', height=300)
-st.markdown('Press `ctrl/cmd + enter` to process input transcript for next step.')
+st.markdown('**Press `ctrl/cmd + enter` to process input transcript for next step.**')
+st.markdown('*Note: There will be a SMOTE error below if you do not fill in the input script above !*')
 
-## Demo code from 7.5-Pipeline.ipynb 
-valid_transcripts_sent_df = pd.read_pickle('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/chloe_valid_transcripts_sent_df')
-valid_transcripts_df = pd.read_pickle('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/valid_transcripts_df')
+valid_transcripts_sent_df = pd.read_pickle('/Users/lihuicham/Documents/GitHub/standup-comedy-analysis/main/chloe_valid_transcripts_sent_df')
+valid_transcripts_df = pd.read_pickle('/Users/lihuicham/Documents/GitHub/standup-comedy-analysis/main/valid_transcripts_df')
 filtered_transcripts_df = filter_by_sentiment(valid_transcripts_df, user_input)
 filtered_transcripts_sent_df = valid_transcripts_sent_df[valid_transcripts_sent_df['Title'].isin(filtered_transcripts_df['Title'])]
 
-vectorizer = TfidfVectorizer()
-filtered_tfidf_matrix = vectorizer.fit_transform(filtered_transcripts_sent_df['Processed Transcript'])
-X_train_filtered = pd.DataFrame(filtered_tfidf_matrix.toarray(), columns = vectorizer.get_feature_names_out())
+full_grad_clf = pd.read_pickle('/Users/lihuicham/Documents/GitHub/standup-comedy-analysis/main/trained_grad_model')
+full_vectorizer = pd.read_pickle('/Users/lihuicham/Documents/GitHub/standup-comedy-analysis/main/full_vectorizer')
+full_features = pd.read_pickle('/Users/lihuicham/Documents/GitHub/standup-comedy-analysis/main/full_features')
+
+filtered_tfidf_matrix = full_vectorizer.transform(filtered_transcripts_sent_df['Processed Transcript'])
+X_train_filtered = pd.DataFrame(filtered_tfidf_matrix.toarray(), columns = full_vectorizer.get_feature_names_out())
+X_train_filtered = X_train_filtered[full_features]
 y_train_filtered = filtered_transcripts_sent_df['Funniness']
 
 smote = SMOTE(k_neighbors = 1)
 X_train_sm, y_train_sm = smote.fit_resample(X_train_filtered, y_train_filtered)
 
-filtered_grad_clf = GradientBoostingClassifier(n_estimators = 80, learning_rate = 0.01, random_state = 0).fit(X_train_sm, y_train_sm)
+filtered_grad_clf = GradientBoostingClassifier(n_estimators = 40, learning_rate = 0.1, random_state = 0).fit(X_train_sm, y_train_sm)
 
-#preprocess 'user_input' and use the vectorizer to transform it
-processed_user_input = preprocess(user_input)
-vec_user_input = vectorizer.transform([processed_user_input])
-user_input_df = pd.DataFrame(vec_user_input.toarray(), columns = vectorizer.get_feature_names_out())
+def weighted_predictions(full_model, filtered_model, X, weight = 0.75):
+    full_probs = full_model.predict_proba(X)
+    filtered_probs = filtered_model.predict_proba(X)
 
-#make prediction of funniness of 'user_input' 
-filtered_pred = filtered_grad_clf.predict(user_input_df)[0]
+    #determine if filtered_model has fitted to all classes
+    unfitted_classes = []
+    for i in range(4):
+        if i not in filtered_model.classes_:
+            unfitted_classes.append(i)
 
-#get model that is pre-trained on all valid transcripts, and also the vectorizer, and features
-full_grad_clf = pickle.load(open('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/trained_grad_model', 'rb'))
-full_vectorizer = pickle.load(open('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/full_vectorizer', 'rb'))
-full_features = pickle.load(open('/Users/lihuicham/Desktop/Y2S2/BT4222/project/standup-comedy-analysis/main/full_features', 'rb'))
+    #add column of zero to filtered_probs for each unfitted class
+    if len(unfitted_classes) > 0:
+        for c in unfitted_classes:
+            filtered_probs = np.insert(filtered_probs, c, 0, axis = 1)
 
-#transform processed 'user_input' using full_vectorizer, select the best 10000 features, and make prediction of funniness using full_grad_clf
-fullvec_user_input = full_vectorizer.transform([processed_user_input])
-full_user_input_df = pd.DataFrame(fullvec_user_input.toarray(), columns = full_vectorizer.get_feature_names_out())
-full_user_input_df = full_user_input_df[full_features]
-full_pred = full_grad_clf.predict(full_user_input_df)[0]
+    adj_probs = (full_probs * weight) + (filtered_probs * (1 - weight))
+    y_hat = adj_probs.argmax(axis = 1)
+    return y_hat
 
+def break_chunks(input, n_chunks):
+    user_tokens = [s for s in input]
+    user_token_count = len(user_tokens)
+    chunk_token_count = user_token_count // n_chunks
+    chunk_ranges = range(0, user_token_count, chunk_token_count)
+    chunks = []
 
-#function to output funniness prediction in word form
+    for c in range(n_chunks):
+        if c == n_chunks - 1:
+            chunk = ''.join(user_tokens[chunk_ranges[c]:])
+        else:
+            chunk = ''.join(user_tokens[chunk_ranges[c]:chunk_ranges[c + 1]])
+        chunks.append(chunk)
+
+    return chunks
+
+user_chunks = break_chunks(user_input, 10) 
+
+chunk_df = pd.DataFrame({'Chunk': user_chunks})
+chunk_df['Processed Chunk'] = chunk_df['Chunk'].apply(preprocess)
+vec_user_input = full_vectorizer.transform(chunk_df['Processed Chunk'])
+user_input_df = pd.DataFrame(vec_user_input.toarray(), columns = full_vectorizer.get_feature_names_out())
+user_input_df = user_input_df[full_features]
+weighted_pred = weighted_predictions(full_grad_clf, filtered_grad_clf, user_input_df)
+chunk_df['Funniness'] = weighted_pred
+
 def get_pred_output(pred):
     if pred == 0:
         return 'Neutral'
@@ -151,32 +210,74 @@ def get_pred_output(pred):
         return 'Moderately funny'
     elif pred == 3:
         return 'Very funny'
-    
-st.write('Funniness based on similar transcripts: ', get_pred_output(filtered_pred))
-st.write('Funniness based on all transcripts available: ', get_pred_output(full_pred))
 
-st.markdown('## Step 2 : Transcripts of similar sentiments')
-filtered_transcripts_df.rename( columns={'Unnamed: 0':'Transcript Index'}, inplace=True )
-st.dataframe(filtered_transcripts_df)
+chunk_df['Funniness Word'] = chunk_df['Funniness'].apply(get_pred_output)
+
+lexicon = Empath()
+def get_topics(text, n_topics):
+    topics_lst = []
+    top_dict = lexicon.analyze(text, normalize = True)
+    sorted_top_lst = sorted(top_dict.items(), key = lambda x: x[1], reverse = True)
+
+    for top_weight in sorted_top_lst[:n_topics]:
+        topics_lst.append(top_weight[0])
+    return topics_lst
+
+filtered_transcripts_df['Topics'] = filtered_transcripts_df['Cleaned Transcript'].apply(get_topics, args = (3,))
+
+chunk_df['Topics'] = chunk_df['Processed Chunk'].apply(get_topics, args = (3,))
+user_topics = get_topics(preprocess(user_input), 3)
+
+'''
+## Step 2 : Transcripts with Similar Sentiment
+The dataframe below shows the transcripts that the your transcript is most similar to in terms of **sentiment** !
+We filter similar transcripts by polarity and subjectivity score. 
+'''
+
+step2_df = filtered_transcripts_df.drop(columns=['Unnamed: 0', 'Date', 'Subtitle', 'Transcript', 'Cleaned Transcript', 'Topics'])
 
 
+st.dataframe(step2_df)
+st.write(f'Your transcript is similar to {len(step2_df.index)} transcripts from our database.')
+
+'''
+## Step 3 : Model Training and Weighted Funniness Predictions 
+Before predicting the funniness level of your transcript, we need to train our model on your transcript.  
+Don't worry, no actions is needed here :) !!
+
+**What is going on behind the scene in this step ?**  
+From Step 2, we have a subset of similar transcripts. We use two models for a **weighted** funniness predictions.
+1. **Full model :** ML model trained on all transcripts. 
+2. **Filtered model :** ML model trained on the subset of similar transcript
+
+Finally, the models are **ensembled and weighted.** 
+'''
+
+'''
+## Step 4 : Breaking into Chunks 
+Okay, your input transcript may have 100000000... lines, and it's too difficult for our model to predict your funniness level accurately.  
+Instead of predicting your entire transcript, why not break them into smaller chunks and we predict the funniness level of each chunk ?  
+**That's much better !!** 
+'''
+step4_df = chunk_df.drop(columns=['Processed Chunk', 'Topics'])
+st.dataframe(step4_df)
 
 
 '''
-# ## Topic Modelling
-# We are using the [**Empath**](https://github.com/Ejhfast/empath-client) library for topic modelling. 
+## Step 5 : Topic Modelling 
+We can do better than just predicting your level of funniness.  
+Let's look at the top 3 topics in your 10 chunks and the transcripts you are similar to. 
+'''
+step5_userchunks = chunk_df.drop(columns=['Processed Chunk', 'Funniness'])
 
-# Let's try this out ! 
-# '''
+step5_similar = filtered_transcripts_df.drop(columns=['Unnamed: 0', 'Date', 'Subtitle', 'Transcript', 'Cleaned Transcript', 'Polarity', 'Subjectivity'])
 
-# sent = st.text_input('Here\'s a sample sentence.', 'He hit the other person.')
+st.markdown('**Top 3 topics from user input transcript chunks :**')
 
-# lexicon = Empath()
+st.dataframe(step5_userchunks)
 
-# def demo(sent) : 
-#     lex_dict_test = lexicon.analyze(sent, normalize=True)
-#     topics = sorted(lex_dict_test, key=lex_dict_test.get, reverse=True)[:5]
-#     return ', '.join(topics)
+st.markdown('**Top 3 topics from each similar transcript  :**')
 
-# st.markdown('**Top 5 Topics :**')
-# st.write(demo(sent))
+st.dataframe(step5_similar)
+
+st.write(f'**Top 3 topics from full transcript (original, before chunking) :** {", ".join(user_topics)}')
