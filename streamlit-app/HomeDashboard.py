fc88e76576decf3b5f1ac3c179e684b9475ee914
Li Hui Cham <lihuicham@gmail.com>
2023-04-10 23:39:45 +0800

    add strealit app link

--- streamlit-app/HomeDashboard.py
+++ streamlit-app/HomeDashboard.py
@@ -5,9 +5,7 @@ import streamlit as st
 
 ### Links : 
 [GitHub Repo & Code](https://github.com/lihuicham/standup-comedy-analysis)  
-[Streamlit Dashboard App]()  
-[Final Project Report]()  
-[Presentation Slides]()  
+[Streamlit Dashboard App](https://lihuicham-standup-come-streamlit-app-deployhomedashboard-16jw8i.streamlit.app/DemoDashboard)  
 
 ### Contributors 
 [Cham Li Hui (@lihuicham)](https://github.com/lihuicham)  
